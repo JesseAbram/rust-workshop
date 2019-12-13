8e0aeff9b52ad80247f7578f359e6278949158fb
JesseAbram <jesseabramowitz@hotmail.com>
2019-12-12 20:00:41 -0500

    added api for exchange rate

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,44 @@
 use actix_web::{web, App, HttpServer};
+use reqwest;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::sync::Mutex;
 
 const INTRO: &str = "Rust Server says hi";
-
+const EXCHANGE_API: &str = "https://api.exchangerate-api.com/v4/latest/CAD";
 struct AppState {
     intro: String,
     counter: Mutex<i64>,
 }
+#[derive(Serialize, Deserialize)]
+struct ExchangeResponse {
+    base: String,
+    date: String,
+    time_last_updated: i32,
+    rates: HashMap<String, f32>,
+}
 
 fn number(number: web::Path<i64>, data: web::Data<AppState>) -> String {
-    let thread = std::thread::current().id();
-    println!("thread: {:?}", thread);
+    // let thread = std::thread::current().id();
+    // println!("thread: {:?}", thread);
     let mut counter = data.counter.lock().unwrap();
     *counter += 1;
 
-    format!("Request Number: {} :: Path: {}", counter, number)
+    format!("Request Number: {} :: Path: {}", counter, number);
+
+    let resp: String = reqwest::get(EXCHANGE_API)
+        .unwrap()
+        .text()
+        .unwrap_or("BAD REASPONSE".to_string());
+
+    // format!("{:?}", resp)
+
+    let v: ExchangeResponse = serde_json::from_str(&resp).unwrap();
+
+    format!(
+        "The exchange rate of 1 {} USE is {} on {}.",
+        v.base, v.rates["USD"], v.date
+    )
 }
 
 fn index(data: web::Data<AppState>) -> String {
